1283575232e7c42d956d6466c8554fd2fed300ce
Ashan-Nilanga <101865264+Ashan-Nilanga@users.noreply.github.com>
2022-03-23 00:52:03 +0530

    navbar-updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,89 +1,91 @@
-import { MenuBook, Search, ShoppingCartOutlined } from "@material-ui/icons";
-import { Badge } from "@mui/material";
+
+import { Badge } from '@material-ui/core';
+import { Search, ShoppingCartOutlined } from '@material-ui/icons';
 import React from 'react'
-import styled from 'styled-components'
+import styled from 'styled-components';
 
-const Container = styled.div`
-    height: 60px;
-    
-`
-
-const Wrapper = styled.div`
-    padding: 10px 20px;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-
-`;
-const Left = styled.div`
-    flex: 1;
-    display: flex;
-    align-items: center;
+const Container =styled.div`
+ height:60px;
 `;
 
-const Language = styled.span`
-    font-size: 14px;
-    cursor: pointer;
+const Wrapper= styled.div`
+ padding: 10px 20px;
+ display: flex;
+ align-items: center;
+ justify-content: space-between;
 `;
 
-const SearchContainer = styled.div`
-    border: 0.5px solid lightgray;
-    display: flex;
-    align-items: center;
-    margin-left: 25x;
-    padding: 5px;
+const Left=styled.div`
+ flex: 1;
+ display: flex;
+ align-items:center;
 `;
 
-const Input = styled.input`
-    border: none;
+const Languages=styled.span`
+ font-size: 14px;
+ cursor: pointer;
 `;
 
-const Center = styled.div`
-    flex: 1;
-    text-align: center;
+const SearchContainer=styled.div`
+ border: 0.5px solid lightgray;
+ display: flex;
+ align-items: center;
+ margin-left: 25px;
+ padding: 5px;
 `;
 
-const Logo = styled.h1`
-    font-weight: bold;
+const Input=styled.input`
+ border: none;
 `;
 
-const Right = styled.div`
-    flex: 1;
-    display: flex;
-    align-items: center;
-    justify-content: flex-end;
+const Center=styled.div`
+ flex: 1;
+ text-align: center;
 `;
 
-const MenuItem = styled.div`
-    font-size: 14px;
-    cursor: pointer;
-    margin-left: 25px;
-
+const Logo=styled.h1`
+ font-weight: bold;
 `;
 
+
+const Right=styled.div`
+ flex: 1;
+ display: flex;
+ align-items: center;
+ justify-content: flex-end;
+ 
+`;
+const Menuitem= styled.div`
+ font-size: 14px;
+ cursor: pointer;
+ margin-left: 25px;
+`;
 
 const Navbar = () => {
   return (
     <Container>
         <Wrapper>
-          <Left>
-             <Language>EN</Language>
-             <SearchContainer>
-                 <Input/>
-                 <Search style={{color:"gray", fontSize:"16px"}}/>
-             </SearchContainer>
-          </Left>
-          <Center><Logo>AShan</Logo></Center>
-          <Right>
-              <MenuItem>REGISTER</MenuItem>
-              <MenuItem>SIGN IN</MenuItem>
-              <MenuItem>
-                 <Badge badgeContent= {4} color="primary">
-                     <ShoppingCartOutlined/>
-                 </Badge>
-                    
-              </MenuItem>
-          </Right>
+
+        <Left>
+            <Languages>En </Languages> 
+            <SearchContainer>
+                <Input/>
+                <Search style={{color:"gray",fontSize:16}}/>
+            </SearchContainer>
+        </Left>
+        <Center><Logo>SHARP.</Logo></Center>
+        <Right>
+            <Menuitem>REGISTER</Menuitem>
+            <Menuitem>SIGN IN</Menuitem>
+            <Menuitem>
+            <Badge badgeContent={4} color="primary">
+<ShoppingCartOutlined/>
+    </Badge>
+            
+            </Menuitem>
+
+        </Right>
+
         </Wrapper>
       
     </Container>
---
+++ src/data.js
@@ -0,0 +1,23 @@
+export const sliderItems = [
+    {
+      id: 1,
+      img: "https://th.bing.com/th/id/OIP.5ds1PIMHnEuicORLeQ7KsgHaKG?pid=ImgDet&rs=1",
+      title: "SUMMER SALE",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "#FFFFFF",
+    },
+    {
+      id: 2,
+      img: "https://i.ibb.co/DG69bQ4/2.png",
+      title: "AUTUMN COLLECTION",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "fcf1ed",
+    },
+    {
+      id: 3,
+      img: "https://i.ibb.co/cXFnLLV/3.png",
+      title: "LOUNGEWEAR LOVE",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "fbf0f4",
+    },
+  ];
