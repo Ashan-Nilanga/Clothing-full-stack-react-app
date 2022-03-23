954eaee3975adb7d8add4cf0e627951daf2d58cc
Ashan-Nilanga <101865264+Ashan-Nilanga@users.noreply.github.com>
2022-03-23 15:39:32 +0530

    Create-Footerjsx

--- src/data.js
+++ src/data.js
@@ -1,85 +1,74 @@
 export const sliderItems = [
-    {
-      id: 1,
-      img: "https://th.bing.com/th/id/OIP.5ds1PIMHnEuicORLeQ7KsgHaKG?pid=ImgDet&rs=1",
-      title: "SUMMER SALE",
-      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
-      bg: "#FFFFFF",
-    },
-    {
-      id: 2,
-      img: "https://i.ibb.co/DG69bQ4/2.png",
-      title: "AUTUMN COLLECTION",
-      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
-      bg: "fcf1ed",
-    },
-    {
-      id: 3,
-      img: "https://i.ibb.co/cXFnLLV/3.png",
-      title: "LOUNGEWEAR LOVE",
-      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
-      bg: "fbf0f4",
-    },
-  ];
-
-
-  export const categories = [ 
-    { 
-      id: 1, 
-      img: "https://images.pexels.com/photos/58836041/pexels-photo-51886041.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940", 
-      title: "SHIRT STYLE!", 
-    },
-     
-    { 
-       id: 2, 
-       img: "https://images.pexels.com/photos/2983464/pexels-photo-2983464.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940", 
-       title: "LOUNGEWEAR LOVE", 
-    }, 
-     { 
-       id: 3, 
-       img: "https://images.pexels.com/photos/54280696/pexels-photo-54806696.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500", 
-       title: "LIGHT JACKETS", 
-    }, 
-    ];
-
-  export const popularProducts = [
-          {
-            id:1,
-            img:"",
-          },
-
-          {
-            id:2,
-            img:"",
-          },
-
-          {
-            id:3,
-            img:"",
-          },
-
-          {
-            id:4,
-            img:"",
-          },
-
-          {
-            id:5,
-            img:"",
-          },
-
-          {
-            id:6,
-            img:"",
-          },
-
-          {
-            id:6,
-            img:"",
-          },
-
-          {
-            id:6,
-            img:"",
-          },
-  ];
+  {
+    id: 1,
+    img: "https://th.bing.com/th/id/OIP.5ds1PIMHnEuicORLeQ7KsgHaKG?pid=ImgDet&rs=1",
+    title: "SUMMER SALE",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "#FFFFFF",
+  },
+  {
+    id: 2,
+    img: "https://i.ibb.co/DG69bQ4/2.png",
+    title: "AUTUMN COLLECTION",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "fcf1ed",
+  },
+  {
+    id: 3,
+    img: "https://i.ibb.co/cXFnLLV/3.png",
+    title: "LOUNGEWEAR LOVE",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "fbf0f4",
+  },
+];
+export const categories = [
+  {
+    id: 1,
+    img: "https://images.pexels.com/photos/5886041/pexels-photo-58860461.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    title: "SHIRT STYLE!",
+  },
+  {
+    id: 2,
+    img: "https://images.pexels.com/photos/2983464/pexels-photo-29836464.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    title: "LOUNGEWEAR LOVE",
+  },
+  {
+    id: 3,
+    img: "https://images.pexels.com/photos/5480696/pexels-photo-54860696.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    title: "LIGHT JACKETS",
+  },
+];
+export const popularProducts = [
+  {
+    id:1,
+    img:"https://d3o2e4jr3mxnm3.cloudfront.net/Mens-Jake-Guittar-Vintage-Crusher-Tee_68382_1_lg.png",
+  },
+  {
+    id:2,
+    img:"https://img01.ztat.net/article/spp-media-p1/a9e33432e4c1347dbe42tb6dc08aa470d/b50dbfd6c5f847b5b699cd653c9ef535.jpg?imwidth=762",
+  },
+  {
+    id:3,
+    img:"https://www.prada.com/content/dam/pradanux_products/U/UCS/UCS319/1YOTF010O/UCS319_1tYOT_F010O_S_182_SLF.png",
+  },
+  {
+    id:4,
+    img:"https://editorialist.com/thumbnails/600/2021/9/015/8864/320/15884320~blue%20multi_0.jpg",
+  },
+  {
+    id:5,
+    img:"https://images.ctfassets.net/5gvckmvm9289/3BlDoZ6xSSjqAvv1jBJP7TH/65f9a95484117730ace42abf64e89572/Noissue-x-Creatsy-Tote-Bag-Mockup-Bundle-_4_-2.png",
+  },
+  {
+    id:6,
+    img:"https://d3o2e4jr3mxnm3.cloudfront.net/Rocket-Vint6age-Chill-Cap_66374_1_lg.png",
+  },
+  {
+    id:7,
+    img:"https://fabletics-us-cdn.justfab.com/media/images/produ6cts/JT2044691-0001/JT2044691-0001-1_998x1498.jpg",
+  },
+  {
+    id:8,
+    img:"https://www.pngarrts.com/files/3/Women-Jacket-PNG-High-Quality-Image.png",
+  },
+];
